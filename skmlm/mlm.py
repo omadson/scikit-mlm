60217b1689b392bb42e94305e429bb46e03af548
Madson Dias <madsonddias@gmail.com>
2019-05-08 16:35:18 -0300

    fix bug of the neighborhood_size

--- skmlm/mlm.py
+++ skmlm/mlm.py
@@ -71,22 +71,23 @@ def mrsr(X, T, norm=1, max_k=None):
 
 # opposite neighborhood
 def ON(X, y, neighborhood_size=1, D_in=None, D_out=None):
+    if neighborhood_size <= X.shape[0]:
+        D_in  = D_in  if isinstance(D_in, np.ndarray)  else sp.spatial.distance.cdist(X,X)
+        D_out = D_out if isinstance(D_out, np.ndarray) else sp.spatial.distance.cdist(y,y)
 
-    D_in  = D_in  if isinstance(D_in, np.ndarray)  else sp.spatial.distance.cdist(X,X)
-    D_out = D_out if isinstance(D_out, np.ndarray) else sp.spatial.distance.cdist(y,y)
-
-    DD = D_out != 0
-    opposite_neighborhood = set()
-    for i in range(D_out.shape[0]):
-        DDD = D_in[i,:].argsort()
-        opposite_neighborhood = opposite_neighborhood.union(set(DDD[DD[i,DDD]][:neighborhood_size]))
-    on_index = np.zeros(X.shape[0])
-    on_index[list(opposite_neighborhood)] = 1
-    if len(opposite_neighborhood) == X.shape[0]:
-        return on_index == 1, D_in, D_out
+        DD = D_out != 0
+        opposite_neighborhood = set()
+        for i in range(D_out.shape[0]):
+            DDD = D_in[i,:].argsort()
+            opposite_neighborhood = opposite_neighborhood.union(set(DDD[DD[i,DDD]][:neighborhood_size]))
+        on_index = np.zeros(X.shape[0])
+        on_index[list(opposite_neighborhood)] = 1
+        if len(opposite_neighborhood) == X.shape[0]:
+            return ON(X, y, neighborhood_size=neighborhood_size-1, D_in=D_in, D_out=D_in)
+        else:
+            return on_index == 1, D_in, D_out
     else:
-        return ON(X, y, neighborhood_size=neighborhood_size-1, D_in=D_in, D_out=D_in):
-
+        return ON(X, y, neighborhood_size=neighborhood_size-1, D_in=D_in, D_out=D_in)
 
 
 # MLM for regression: https://doi.org/10.1016/j.neucom.2014.11.073
