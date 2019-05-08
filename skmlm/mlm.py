aee8e5ed163507948567b0e7c709055eb1e531ad
Madson Dias <madsonddias@gmail.com>
2019-05-08 10:30:20 -0300

    code refactoring

--- skmlm/mlm.py
+++ skmlm/mlm.py
@@ -69,6 +69,22 @@ def mrsr(X, T, norm=1, max_k=None):
         
     return W_k,list(A)
 
+# opposite neighborhood
+def ON(X, y, neighborhood_size=1, D_in=None, D_out=None):
+
+    D_in  = D_in  if isinstance(D_in, np.ndarray)  else sp.spatial.distance.cdist(X,X)
+    D_out = D_out if isinstance(D_out, np.ndarray) else sp.spatial.distance.cdist(y,y)
+
+    DD = D_out != 0
+    opposite_neighborhood = set()
+    for i in range(D_out.shape[0]):
+        DDD = D_in[i,:].argsort()
+        opposite_neighborhood = opposite_neighborhood.union(set(DDD[DD[i,DDD]][:neighborhood_size]))
+    on_index = np.zeros(X.shape[0])
+    on_index[list(opposite_neighborhood)] = 1
+    return on_index == 1, D_in, D_out
+
+
 
 # MLM for regression: https://doi.org/10.1016/j.neucom.2014.11.073
 class MLMR(BaseEstimator, RegressorMixin):
@@ -83,13 +99,13 @@ class MLMR(BaseEstimator, RegressorMixin):
             self.rp_number = int(np.ceil(0.1 * X.shape[0]))
         self.rp_index = np.random.choice(X.shape[0], self.rp_number, replace=False)
         self.rp_X     = X[self.rp_index,:]
-        self.rp_y     = y[self.rp_index]
+        self.rp_y     = y[self.rp_index,:]
 
         # compute pairwise distance matrices
         #  - D_in: input space
         #  - D_out: output space
         self.D_in  = sp.spatial.distance.cdist(X,self.rp_X)
-        self.D_out = sp.spatial.distance.cdist(y[np.newaxis].T,self.rp_y[np.newaxis].T)
+        self.D_out = sp.spatial.distance.cdist(y,self.rp_y)
 
         # compute the distance regression matrix using OLS
         self.B = np.linalg.pinv(self.D_in).dot(self.D_out)
@@ -106,64 +122,8 @@ class MLMR(BaseEstimator, RegressorMixin):
 
     def get_output(self, x):
         J = lambda y: self.in_cost(y, x)
-        out = sp.optimize.least_squares(J, x0=self.rp_y.mean(), method='lm')
-        return out.x
-
-    def in_cost(self, y, x):
-        """internal cost function"""
-        # make y a vector
-        y  = np.array([y])
-
-        # compute pairwise distance vectors
-        #  - d_in: input space
-        #  - d_out: output space
-        d_in  = sp.spatial.distance.cdist(x[np.newaxis],self.rp_X)
-        d_out = sp.spatial.distance.cdist(y.T,self.rp_y[np.newaxis].T)
-
-        # compute the internal cost function
-        return (d_out**2 - d_in.dot(self.B)**2)[0]
-
-# MLM for classification: https://doi.org/10.1016/j.neucom.2014.11.073
-class MLMC(BaseEstimator, ClassifierMixin):
-    def __init__(self, rp_number=None):
-
-        # number of reference points
-        self.rp_number = rp_number
-
-    def fit(self, X, y=None):
-        # convert outputs to one-hot encoding
-        y = one_hot(y) if len(y.shape) == 1 else y
-
-        # random select of reference points for inputs and outputs
-        if self.rp_number == None:
-            self.rp_number = int(np.ceil(0.1 * X.shape[0]))
-        self.rp_index = np.random.choice(X.shape[0], self.rp_number, replace=False)
-        self.rp_X     = X[self.rp_index,:]
-        self.rp_y     = y[self.rp_index,:]
-
-        # compute pairwise distance matrices
-        #  - D_in: input space
-        #  - D_out: output space
-        self.D_in  = sp.spatial.distance.cdist(X,self.rp_X)
-        self.D_out = sp.spatial.distance.cdist(y,self.rp_y)
-
-        # compute the distance regression matrix using OLS
-        self.B = np.linalg.pinv(self.D_in).dot(self.D_out)
-
-        return self
-
-    def predict(self, X, y=None):
-        try:
-            getattr(self, "B")
-        except AttributeError:
-            raise RuntimeError("You must train classifer before predicting data!")
-
-        return np.array([self.get_output(x) for x in X])
-
-    def get_output(self, x):
-        J = lambda y: self.in_cost(y, x)
         out = sp.optimize.least_squares(J, x0=self.rp_y.mean(axis=0), method='lm')
-        return np.argmax(out.x)
+        return out.x
 
     def in_cost(self, y, x):
         """internal cost function"""
@@ -179,45 +139,32 @@ class MLMC(BaseEstimator, ClassifierMixin):
         # compute the internal cost function
         return (d_out**2 - d_in.dot(self.B)**2)[0]
 
-
-# nearest neighbor MLM (NN-MLM): https://link.springer.com/article/10.1007%2Fs11063-017-9587-5#Sec9
-class NN_MLM(BaseEstimator, ClassifierMixin):
-    def __init__(self, rp_number=None):
-
-        # number of reference points
-        self.rp_number = rp_number
-
-    
-
+# MLM for classification: https://doi.org/10.1016/j.neucom.2014.11.073
+class MLMC(MLMR):
     def fit(self, X, y=None):
         # convert outputs to one-hot encoding
         y = one_hot(y) if len(y.shape) == 1 else y
-
-        # random select of reference points for inputs and outputs
-        if self.rp_number == None:
-            self.rp_number = int(np.ceil(0.1 * X.shape[0]))
-        self.rp_index = np.random.choice(X.shape[0], self.rp_number, replace=False)
-        self.rp_X     = X[self.rp_index,:]
-        self.rp_y     = y[self.rp_index,:]
-
-        # compute pairwise distance matrices
-        #  - D_in: input space
-        #  - D_out: output space
-        self.D_in  = sp.spatial.distance.cdist(X,self.rp_X)
-        self.D_out = sp.spatial.distance.cdist(y,self.rp_y)
-
-        # compute the distance regression matrix using OLS
-        self.B = np.linalg.pinv(self.D_in).dot(self.D_out)
-
-        return self
+        # fit model using MLMR procedure
+        return super().fit(X,y)
 
     def predict(self, X, y=None):
         try:
             getattr(self, "B")
         except AttributeError:
             raise RuntimeError("You must train classifer before predicting data!")
+        return np.array([np.argmax(self.get_output(x)) for x in X])
 
-        y_hat = list()
+    
+
+
+# nearest neighbor MLM (NN-MLM): https://link.springer.com/article/10.1007%2Fs11063-017-9587-5#Sec9
+class NN_MLM(MLMC):
+    def predict(self, X, y=None):
+        try:
+            getattr(self, "B")
+        except AttributeError:
+            raise RuntimeError("You must train classifer before predicting data!")
+
         # compute matrix of distances from input RPs
         D_in = sp.spatial.distance.cdist(X,self.rp_X)
         # estimate matrix of distances from output RPs
@@ -233,33 +180,19 @@ class ON_MLM(NN_MLM):
         # number of reference points
         self.neighborhood_size = neighborhood_size
 
-    def ON(self, X, y, neighborhood_size=1, D_in=None, D_out=None):
-        
-        D_in  = D_in  if isinstance(D_in, np.ndarray)  else sp.spatial.distance.cdist(X,X)
-        D_out = D_out if isinstance(D_out, np.ndarray) else sp.spatial.distance.cdist(y,y)
-
-        DD = D_out != 0
-        opposite_neighborhood = set()
-        for i in range(D_out.shape[0]):
-            DDD = D_in[i,:].argsort()
-            opposite_neighborhood = opposite_neighborhood.union(set(DDD[DD[i,DDD]][:neighborhood_size]))
-        on_index = np.zeros(X.shape[0])
-        on_index[list(opposite_neighborhood)] = 1
-        return on_index == 1, D_in, D_out
-
     def fit(self, X, y=None):
         # convert outputs to one-hot encoding
         y = one_hot(y) if len(y.shape) == 1 else y
 
         # opposite neighborhood procedure
         # first time
-        on_index_1, D_in, D_out = self.ON(X, y, neighborhood_size=self.neighborhood_size)
+        on_index_1, D_in, D_out = ON(X, y, neighborhood_size=self.neighborhood_size)
         # second time
-        on_index_2,_,_ = self.ON(X[~on_index_1,:],
-                             y[~on_index_1,:],
-                             neighborhood_size=1,
-                             D_in=D_in[~on_index_1,:][:,~on_index_1],
-                             D_out=D_out[~on_index_1,:][:,~on_index_1])
+        on_index_2,_,_ = ON(X[~on_index_1,:],
+                            y[~on_index_1,:],
+                            neighborhood_size=1,
+                            D_in=D_in[~on_index_1,:][:,~on_index_1],
+                            D_out=D_out[~on_index_1,:][:,~on_index_1])
 
         self.rp_index = np.array([i for i, x in enumerate(~on_index_1) if x])[[i for i, x in enumerate(on_index_2) if x]]
 
@@ -282,10 +215,6 @@ class ON_MLM(NN_MLM):
 
 
 class w_MLM(NN_MLM):
-    def __init__(self, rp_number=None):
-
-        # number of reference points
-        self.rp_number = rp_number
 
     def fit(self, X, y=None):
         # convert outputs to one-hot encoding
@@ -307,7 +236,6 @@ class w_MLM(NN_MLM):
         #  - D_out: output space
         self.D_in  = sp.spatial.distance.cdist(X,self.rp_X)
         self.D_out = sp.spatial.distance.cdist(y,self.rp_y)
-
 
 
         self.W = np.diag(w)
