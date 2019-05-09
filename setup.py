9751d96da4b2771783fc0288e7dc69354082796a
Madson Dias <madsonddias@gmail.com>
2019-05-09 15:32:16 -0300

    fcm test install 2 return

--- setup.py
+++ setup.py
@@ -37,9 +37,7 @@ setup(
         'numpy>=1.15.2',
         'scipy>=1.1.0',
         'scikit-learn>=0.19.1',
-        'fuzzycmeans>1.0.1'
     ],
-    dependency_links = ['https://github.com/omadson/fuzzy-c-means/tarball/master'],
     zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
