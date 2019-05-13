82ab44db10ca547a58bd64fe10057ab181cc8eb0
Madson Dias <madsonddias@gmail.com>
2019-05-13 10:50:04 -0300

    change version

--- setup.py
+++ setup.py
@@ -8,25 +8,9 @@ from setuptools import setup, find_packages
 with open("DESCRIPTION.md", "r") as fh:
     long_description = fh.read()
 
-version = '0.0.3'
-
-if sys.argv[-1] == 'publish':
-    if os.system("pip freeze | grep wheel"):
-        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
-        sys.exit()
-    if os.system("pip freeze | grep twine"):
-        print("twine not installed.\nUse `pip install twine`.\nExiting.")
-        sys.exit()
-    os.system("python setup.py sdist bdist_wheel")
-    os.system("twine upload dist/*")
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %s -m 'version %s'" % (version, version))
-    print("  git push --tags")
-    sys.exit()
-
 setup(
     name='scikit-mlm',
-    version=version,
+    version='0.0.4',
     description='Minimal Learning Machine implementation using the scikit-learn API',
     long_description=long_description,
     long_description_content_type="text/markdown",
